9a0b0a12a5b64b46f91d2ccaafeaf0a24fb37ccb
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-06-05 23:56:47 +0800

    clean .gitignore remove cache :fire:

--- cache/1b71a1e91812a3b32f3b4c28b47c9ba97f6b5707.php
+++
@@ -1,3 +0,0 @@
-<?php $__env->startSection('content'); ?><h3>Start write your amazing posts...</h3>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('_layouts.post', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
--- cache/fd615d69d4cf37262bfd84e500d266e9a70f1e88.php
+++
@@ -1,3 +0,0 @@
-<?php $__env->startSection('content'); ?><p>These posts are all related to css</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('_layouts.category', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
